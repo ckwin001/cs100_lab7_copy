6d584f1e07a7ba7044228b83337334267992ea4f
ckwin001 <ckwin001@ucr.edu>
2021-05-23 17:33:31 -0700

    Added non-buggy failing + multi op tests

--- tests/factory_test.hpp
+++ tests/factory_test.hpp
@@ -61,5 +61,39 @@ TEST(FactoryTest, simplePow ) {
     EXPECT_NEAR(9, op->evaluate(), 0.001);
 }
 
+TEST(FactoryTest, fail_InvalidChar ) {
+    Factory test;
+    char *args[]={"$", "-", "3"};
+    Base *op = test.parse(args, 3);
+    EXPECT_TRUE(op == nullptr);
+}
+
+TEST(FactoryTest, fail_ExtraOperand ) {
+    Factory test;
+    char *args[]={"3", "3", "3"};
+    Base *op = test.parse(args, 3);
+    EXPECT_TRUE(op == nullptr);
+}
+
+TEST(FactoryTest, AddSub ) {
+    Factory test;
+    char *args[]={"5", "+", "3", "-", "4"};
+    Base *op = test.parse(args, 5);
+    EXPECT_NEAR(4, op->evaluate(), 0.001);
+}
+
+TEST(FactoryTest, MultDiv ) {
+    Factory test;
+    char *args[]={"4", "*", "6", "/", "3"};
+    Base *op = test.parse(args, 5);
+    EXPECT_NEAR(8, op->evaluate(), 0.001);
+}
+
+TEST(FactoryTest, AddSubPow ) {
+    Factory test;
+    char *args[]={"1", "+", "7", "-", "6", "**", "5"};
+    Base *op = test.parse(args, 7);
+    EXPECT_NEAR(32, op->evaluate(), 0.001);
+}
 
 #endif
